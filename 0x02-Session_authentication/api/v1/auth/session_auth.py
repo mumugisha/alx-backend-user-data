1a5076dfa89a9ed0775bb6c2f36b199736e6acec
mumugisha <prudent.nkubito@gmail.com>
2024-11-14 13:04:09 +0200

    Session authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -23,11 +23,11 @@ class SessionAuth(Auth):
         """
         if user_id is None or not isinstance(user_id, str):
             return None
-        
+
         session_id = str(uuid4())
-        
+
         self.user_id_by_session_id[session_id] = user_id
-        
+
         return session_id
 
     def user_id_for_session_id(self, session_id: str = None) -> str:
