3d36dfea359d20cd6ba9d7114eab2dfa57f6d05a
mumugisha <prudent.nkubito@gmail.com>
2024-11-14 21:38:04 +0200

    Session authentication

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -1,24 +1,11 @@
 #!/usr/bin/env python3
+""" DocDocDocDocDocDoc
 """
-This module initializes the app views for the API.
-It sets up the URL prefix for version 1 of the API and imports
-the necessary routes for index, users, and session authentication.
-"""
-
-# Importing required modules and functions at the top
 from flask import Blueprint
-from api.v1.views.index import index_view
-from api.v1.views.users import users_view
-from api.v1.views.session_auth import session_auth_view
-from api.v1.models.user import User
 
-# Creating a Blueprint instance for app views
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-# Register the views with the blueprint (to be added at the end of the file)
-app_views.add_url_rule("/", view_func=index_view)
-app_views.add_url_rule("/users", view_func=users_view)
-app_views.add_url_rule("/session", view_func=session_auth_view)
+from api.v1.views.index import *
+from api.v1.views.users import *
 
-# Load user data from a file, if required by the app
 User.load_from_file()
