e98d55ee1fb299cd2139cd3ddbf5a6ca0a158a2c
mumugisha <prudent.nkubito@gmail.com>
2024-11-16 13:54:11 +0200

    Session authentication

--- 0x02-Session_authentication/api/v1/views/session_auth.py
+++ 0x02-Session_authentication/api/v1/views/session_auth.py
@@ -4,7 +4,7 @@ Session ID, you can request all protected API
 routes by using this Session ID
 """
 
-import os
+from os import getenv
 from flask import jsonify, request, abort
 from api.v1.views import app_views
 from models.user import User
@@ -32,7 +32,7 @@ def auth_session():
             from api.v1.app import auth
             session_id = auth.create_session(user.id)
             resp = jsonify(user.to_json())
-            session_name = os.getenv('SESSION_NAME')
+            session_name = getenv('SESSION_NAME')
             resp.set_cookie(session_name, session_id)
             return resp
     return jsonify({"error": "wrong password"}), 401
