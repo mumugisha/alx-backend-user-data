d95b8eb74d2fbe935dd7fee0d3fc0a6471d65266
mumugisha <prudent.nkubito@gmail.com>
2024-11-21 13:21:16 +0200

    Üser authentication services

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -20,7 +20,7 @@ def users() -> str:
     email = request.form.get("email")
     password = request.form.get("password")
     try:
-        user = AUTH.register_user(email, password)
+        AUTH.register_user(email, password)
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": email, "message": "user created"}), 201
