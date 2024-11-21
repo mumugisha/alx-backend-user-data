3d213146e22f4e8885ac69b0b75fbc8cd99e1666
mumugisha <prudent.nkubito@gmail.com>
2024-11-21 13:35:41 +0200

    Üser authentication services

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,16 +1,16 @@
 #!/usr/bin/env python3
-""" 
+"""
 Flask app
 """
 
 from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
+
 
 class DB:
     def __init__(self) -> None:
