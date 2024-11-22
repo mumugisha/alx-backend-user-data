4a368a07e41ff487992989b51368e00a37c2a2f8
Alphonse Mugisha <79317323+mumugisha@users.noreply.github.com>
2024-11-22 03:44:44 +0200

    Update db.py

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,90 +1,110 @@
 #!/usr/bin/env python3
-""" Flask app
 """
-from auth import Auth
-from flask import Flask, abort, jsonify, request, redirect
+DB module
+"""
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
-app = Flask(__name__)
-AUTH = Auth()
-
-
-@app.route("/", methods=['GET'], strict_slashes=False)
-def index() -> str:
-    """ Return JSON response: {"message": "Bienvenue"} """
-    return jsonify({"message": "Bienvenue"})
-
-
-@app.route("/users", methods=['POST'], strict_slashes=False)
-def users() -> str:
-    """ Register new users """
-    email = request.form.get("email")
-    password = request.form.get("password")
-    try:
-        user = AUTH.register_user(email, password)
-    except ValueError:
-        return jsonify({"message": "email already registered"}), 400
-    return jsonify({"email": email, "message": "user created"}), 201
+from user import Base, User
 
 
-@app.route("/sessions", methods=["POST"], strict_slashes=False)
-def login() -> str:
-    """ Log in a user if credentials are correct """
-    email = request.form.get("email")
-    password = request.form.get("password")
+class DB:
+    """DB class"""
 
-    if not AUTH.valid_login(email, password):
-        abort(401)
+    def __init__(self) -> None:
+        """
+        Initialize a new DB instance.
+        """
+        self._engine = create_engine(
+            "sqlite:///a.db", echo=False
+        )
+        Base.metadata.drop_all(self._engine)
+        Base.metadata.create_all(self._engine)
+        self.__session = None
 
-    session_id = AUTH.create_session(email)
-    response = jsonify({"email": email, "message": "logged in"})
-    response.set_cookie("session_id", session_id)
-    return response
+    @property
+    def _session(self) -> Session:
+        """
+        Memoized session object.
+        """
+        if self.__session is None:
+            DBSession = sessionmaker(bind=self._engine)
+            self.__session = DBSession()
+        return self.__session
 
+    def add_user(self, email: str, hashed_password: str) -> User:
+        """
+        Create a User object and save it to the database.
 
-@app.route("/sessions", methods=['DELETE'], strict_slashes=False)
-def logout():
-    """ Log out a user and destroy their session """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user is None or session_id is None:
-        abort(403)
-    AUTH.destroy_session(user.id)
-    return redirect("/")
+        Args:
+            email (str): User's email address.
+            hashed_password (str): Hashed password.
 
+        Returns:
+            User: Newly created User object.
+        """
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
+        self._session.commit()
+        return user
 
-@app.route("/profile", methods=['GET'], strict_slashes=False)
-def profile() -> str:
-    """ Return user's email based on session_id """
-    session_id = request.cookies.get("session_id")
-    user = AUTH.get_user_from_session_id(session_id)
-    if user:
-        return jsonify({"email": user.email}), 200
-    abort(403)
+    def find_user_by(self, **kwargs) -> User:
+        """
+        Find a user by attributes.
 
+        Args:
+            **kwargs: Arbitrary keyword arguments representing attributes
+                      to match the user.
 
-@app.route("/reset_password", methods=['POST'], strict_slashes=False)
-def get_reset_password_token() -> str:
-    """ Generate a token for resetting user's password """
-    email = request.form.get("email")
-    try:
-        reset_token = AUTH.get_reset_password_token(email)
-    except ValueError:
-        abort(403)
-    return jsonify({"email": email, "reset_token": reset_token})
+        Returns:
+            User: User object if a match is found.
 
+        Raises:
+            InvalidRequestError: If any attribute is invalid.
+            NoResultFound: If no user matches the query.
+        """
+        all_users = self._session.query(User)
+        for key, value in kwargs.items():
+            if key not in User.__dict__:
+                raise InvalidRequestError(
+                    f"Invalid attribute: {key}"
+                )
+            for user in all_users:
+                if getattr(user, key) == value:
+                    return user
+        raise NoResultFound(
+            "No user found with the given attributes."
+        )
 
-@app.route("/update_password", methods=['POST'], strict_slashes=False)
-def update_password() -> str:
-    """ Update user's password """
-    email = request.form.get("email")
-    reset_token = request.form.get("reset_token")
-    new_password = request.form.get("new_password")
-    try:
-        AUTH.update_password(reset_token, new_password)
-    except ValueError:
-        abort(403)
-    return jsonify({"email": email, "message": "password updated"})
+    def update_user(self, user_id: int, **kwargs) -> None:
+        """
+        Update a user's attributes.
 
+        Args:
+            user_id (int): User's ID.
+            **kwargs: Key-value pairs representing attributes and new values.
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+        Raises:
+            ValueError: If user does not exist or invalid attribute is passed.
+
+        Returns:
+            None
+        """
+        try:
+            user = self.find_user_by(id=user_id)
+        except NoResultFound:
+            raise ValueError(
+                f"No user found with ID {user_id}"
+            )
+        for key, value in kwargs.items():
+            if hasattr(user, key):
+                setattr(user, key, value)
+            else:
+                raise ValueError(
+                    f"Invalid attribute: {key}"
+                )
+        self._session.commit()
